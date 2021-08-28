55ef06adfe10dd8f9d3dee5f5ec2f99b7a37183c
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-08-28 19:44:09 -0400

    Changed Achievements Page (caps)

--- src/content/Achievements.tsx
+++ src/content/Achievements.tsx
@@ -3,37 +3,41 @@ import Page, { Description } from "../components/Page";
 
 const Achievements = () => (
   <Page title="Achievements">
-    <Description>I have competed in numerous academic olympiads.</Description>
+    <Description>
+      I have competed in numerous academic olympiads and competitions as of the
+      date of writing.
+    </Description>
     <CardGroup>
       <Card
-        title="2021 USAJMO Qualification"
+        title="2021 USAJMO Qualifier"
         href="https://www.maa.org/sites/default/files/pdf/AMC/usamo/2021/JMO%20List.pdf"
       >
-        In the 2020-2021 Mathematical Association of America season, I scored a
-        141 on the AMC 10A and a 10/15 on the AIME II, which qualified me for
-        the 2021 USAJMO.
+        In the 2020-2021 Mathematical Association of America competition season,
+        I scored a 141 on the AMC 10A and a 10/15 on the AIME II, which
+        qualified me for the 2021 United States of America Junior Mathematical
+        Olympiad.
       </Card>
       <Card
         title="USACO Platinum Competitor"
         href="http://usaco.org/current/data/jan21_gold_results.html"
       >
-        In the 2021 January USACO Contest, I scored a 917/1000 in the Gold
-        division and was promoted to the platinum division.
+        In the 2021 January USA Computing Olympiad Contest, I scored a 917/1000
+        in the Gold division and was promoted to the Platinum Division.
       </Card>
-      <Card title="2021 USNCO National Exam">
-        I qualified for the National exam in the US National Chemistry Olympiad
-        through the silicon valley division.
+      <Card title="2021 USNCO National Exam Qualifier">
+        I qualified for the National Exam in the US National Chemistry Olympiad
+        through the Silicon Valley Division.
       </Card>
       <Card
-        title="2021 ACSL Gold"
+        title="2021 ACSL Gold Medalist"
         href="https://www.scores.acsl.org/leaderboard/finals/i/g/"
       >
-        I receieved a Gold Medal in the National Final for the Intermediate
-        Division of the American Computer Science League (ACSL) by scoring 40/40
-        points.
+        I receieved a Gold Medal in the National Final of the Intermediate
+        Division of the American Computer Science League (ACSL) by scoring 40
+        out of 40 points.
       </Card>
       <Card
-        title="2021 Canadian Computing Competition"
+        title="Group 2 2021 Canadian Computing Competition"
         href="https://cemc.uwaterloo.ca/contests/past_contests/2021/2021CCCResults.pdf"
       >
         I finished in the top 75 out of 2920 competitors in the Senior Division
