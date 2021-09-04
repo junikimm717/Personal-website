d4ae58c7cd10e65ecca4b94d16ce2d2afcf4e7f5
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-09-04 00:39:56 -0400

    updated projects

--- src/content/Projects.tsx
+++ src/content/Projects.tsx
@@ -1,36 +1,40 @@
-import { Card, CardGroup } from "../components/Card";
-import Page, { Description } from "../components/Page";
+import { Card, CardGroup } from '../components/Card';
+import Page, { Description } from '../components/Page';
 
 const Projects = () => (
-  <Page title="Projects">
-    <Description>
-      Below are some notable/landmark projects I have done so far
-    </Description>
-    <CardGroup>
-      <Card
-        title="Patternmatch"
-        href="https://git.junickim.me/junikimm717/patternmatch"
-      >
-        An R Script designed to help filter halogenated exposome clusters in
-        linear time (has multicore support on UNIX)
-      </Card>
-      <Card title="mt" href="https://gitlab.com/jpkg/mt">
-        A Perl script for automatically opening meetings using a TOML
-        configuration file
-      </Card>
-      <Card
-        title="Latin 1 Final Project"
-        href="https://github.com/junikimm717/OLA11-Final-Project"
-      >
-        A rudimentary API written in Flask + Pymongo that can automatically
-        download latin vocabulary and conjugate it (only works for regular verbs
-        and nouns)
-      </Card>
-      <Card title="Personal Website">
-        This website, written in React and SCSS
-      </Card>
-    </CardGroup>
-  </Page>
+	<Page title='Projects'>
+		<Description>
+			Below are some notable/landmark projects I have done so far
+		</Description>
+		<CardGroup>
+			<Card
+				title='Patternmatch'
+				href='https://git.junickim.me/junikimm717/patternmatch'
+			>
+				An R Script designed to help filter halogenated exposome clusters in
+				linear time (has multicore support on UNIX)
+			</Card>
+			<Card title='mt' href='https://gitlab.com/jpkg/mt'>
+				A Perl script for automatically opening meetings using a TOML
+				configuration file
+			</Card>
+			<Card
+				title='Latin 1 Final Project'
+				href='https://github.com/junikimm717/OLA11-Final-Project'
+			>
+				A rudimentary API written in Flask + Pymongo that can automatically
+				download latin vocabulary and conjugate it (only works for regular verbs
+				and nouns)
+			</Card>
+			<Card title='Personal Website'>
+				This website, written in React and SCSS
+			</Card>
+			<Card title='MAA Tester' href='https://amcaimetester.netlify.app'>
+				A React/Firebase Web App that can store and automatically grade AMC and
+				AIME exams.
+			</Card>
+		</CardGroup>
+	</Page>
 );
 
 export default Projects;
