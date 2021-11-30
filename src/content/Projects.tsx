7409c51587d6d0ae4b8fa05b71524842f05d4d2b
junikimm717 <junikimm717@gmail.com>
2021-11-30 18:47:07 -0500

    projects updated

--- src/content/Projects.tsx
+++ src/content/Projects.tsx
@@ -7,6 +7,13 @@ const Projects = () => (
       Below are some notable/landmark projects I have done so far
     </Description>
     <CardGroup>
+      <Card
+        title="Go-stop datacollection"
+        href="https://github.com/gostoptools/datacollection"
+      >
+        A complete expressjs REST API and Gatsby Client to collect data in games
+        of Go-Stop.
+      </Card>
       <Card title="MAA Tester" href="https://maatester.com">
         A Web App for keeping track of progress while preparing for math
         contests.
@@ -16,19 +23,17 @@ const Projects = () => (
         href="https://git.junickim.me/junikimm717/patternmatch"
       >
         An R Script designed to help filter HRMS Peaks with halogenated
-        compounds in linear time (has multicore support on UNIX)
+        compounds in roughly linear time (with multicore support)
       </Card>
-      <Card title="mt" href="https://gitlab.com/jpkg/mt">
-        A Perl script for automatically opening meetings using a TOML
-        configuration file
+      <Card title="mt" href="https://github.com/junikimm717/mt">
+        A Rust program for joining zoom meetings through a TOML configuration
+        file.
       </Card>
       <Card
         title="Latin 1 Final Project"
         href="https://github.com/junikimm717/OLA11-Final-Project"
       >
-        A rudimentary API written in Flask + Pymongo that can automatically
-        download latin vocabulary and conjugate it (only works for regular verbs
-        and nouns)
+        Flask + Pymongo API with a dead-simple package manager
       </Card>
       <Card title="Personal Website">
         This website, written in React and SCSS
