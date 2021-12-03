28508490964f79ee33196e7db89b2c9c0e50ff6b
junikimm717 <junikimm717@gmail.com>
2021-12-03 16:13:52 -0500

    dumb ci bug

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import{BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from '../content/Home';
@@ -7,34 +7,15 @@ import Contact from '../content/Contact';
 import Projects from '../content/Projects';
 import School from '../content/School';
 import NotFound from '../content/404';
-import Particles from './Particles';
+// import Particles from './Particles';
 
 function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Switch>
-				<Route path='/achievements'>
-					<Achievements />
-				</Route>
-				<Route path='/school'>
-					<School />
-				</Route>
-				<Route path='/projects'>
-					<Projects />
-				</Route>
-				<Route path='/contact'>
-					<Contact />
-				</Route>
-				<Route exact path='/'>
-					<Home />
-				</Route>
-				<Route path='*'>
-					<NotFound />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (<Router><Navbar /><Switch><Route path = '/achievements'>
+          <Achievements /></ Route><Route path = '/school'><School /></ Route>
+          <Route path = '/projects'><Projects /></ Route>
+          <Route path = '/contact'><Contact /></ Route><Route exact path = '/'>
+          <Home /></ Route><Route path = '*'><NotFound /></ Route></ Switch>
+          </ Router>);
 }
 
 export default App;
