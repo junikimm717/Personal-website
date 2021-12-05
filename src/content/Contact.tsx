75bc66a707b967bf7aa601ca0c95de0fa29cae31
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-12-04 21:29:45 -0500

    go-stop added

--- src/content/Contact.tsx
+++ src/content/Contact.tsx
@@ -1,28 +1,24 @@
-import Page, { Description } from "../components/Page";
+import Page, { Description } from '../components/Page';
 
 const Contact = () => (
-  <Page title="Contact">
-    <Description>
-      <ul>
-        <li>
-          {" "}
-          <a href="https://github.com/junikimm717"> Github </a>
-        </li>
-        <li>
-          {" "}
-          <a href="https://gitlab.com/junikimm717"> Gitlab </a>
-        </li>
-        <li>
-          {" "}
-          <a href="https://git.junickim.me/junikimm717"> Gitea Instance </a>
-        </li>
-        <li>
-          {" "}
-          <a href="mailto:junikimm717@gmail.com"> Email </a>
-        </li>
-      </ul>
-    </Description>
-  </Page>
+	<Page title='Contact'>
+		<Description>
+			<ul>
+				<li>
+					<a href='https://github.com/junikimm717'> Github </a>
+				</li>
+				<li>
+					<a href='https://gitlab.com/junikimm717'> Gitlab </a>
+				</li>
+				<li>
+					<a href='https://www.instagram.com/junichristopherkim'> Instagram </a>
+				</li>
+				<li>
+					<a href='mailto:junikimm717@gmail.com'> Email </a>
+				</li>
+			</ul>
+		</Description>
+	</Page>
 );
 
 export default Contact;
--- src/content/Home.tsx
+++ src/content/Home.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import Header from "../components/Header";
-import Page, { Description } from "../components/Page";
+import React from 'react';
+import Header from '../components/Header';
+import Page, { Description } from '../components/Page';
 
 const Home = () => (
-  <React.Fragment>
-    <Header />
-    <Page title="About Me">
-      <Description>
-        I am a sophomore at Stanford Online High School who is interested in
-        Unix-like operating systems, web development, and general-purpose
-        scripting. Since my first encounter with programming via the USA
-        Computing Olympiad, I have explored beyond the bounds of competitive
-        programming in a journey to find myself. <br /> <br /> Outside of
-        programming, I participate extensively in competition math and train
-        casually in the sports of fencing and boxing.
-      </Description>
-    </Page>
-  </React.Fragment>
+	<React.Fragment>
+		<Header />
+		<Page title='About Me'>
+			<Description>
+				I am a sophomore at Stanford Online High School who is interested in
+				Unix-like operating systems, web development, and general-purpose
+				scripting. Since my first encounter with programming via the USA
+				Computing Olympiad, I have explored beyond the bounds of competitive
+				programming in a journey to find myself. <br /> <br /> Outside of
+				programming, I participate extensively in competition math and train
+				casually in the sports of fencing and boxing.
+				<br /> <br /> I also have a particular affinity for the Korean game of
+				Go-Stop, which I play on a regular basis.
+			</Description>
+		</Page>
+	</React.Fragment>
 );
 
 export default Home;
