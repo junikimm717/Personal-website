6f318632b1621bda3ef46d27c63b61babb00aeb3
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-12-22 14:07:58 -0500

    restored yarn.lock

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import{BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from '../content/Home';
@@ -7,15 +7,36 @@ import Contact from '../content/Contact';
 import Projects from '../content/Projects';
 import School from '../content/School';
 import NotFound from '../content/404';
-// import Particles from './Particles';
+import Particles from './Particles';
 
 function App() {
-  return (<Router><Navbar /><Switch><Route path = '/achievements'>
-          <Achievements /></ Route><Route path = '/school'><School /></ Route>
-          <Route path = '/projects'><Projects /></ Route>
-          <Route path = '/contact'><Contact /></ Route><Route exact path = '/'>
-          <Home /></ Route><Route path = '*'><NotFound /></ Route></ Switch>
-          </ Router>);
+	return (
+		<Router>
+			<Navbar />
+			<Particles>
+				<Switch>
+					<Route path='/achievements'>
+						<Achievements />
+					</Route>
+					<Route path='/school'>
+						<School />
+					</Route>
+					<Route path='/projects'>
+						<Projects />
+					</Route>
+					<Route path='/contact'>
+						<Contact />
+					</Route>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route path='*'>
+						<NotFound />
+					</Route>
+				</Switch>
+			</Particles>
+		</Router>
+	);
 }
 
 export default App;
